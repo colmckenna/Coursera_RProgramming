132ca7926b56e21183392a2f9c1ae9f2ce6dcdde
Colin M <col726@gmail.com>
2014-05-14 09:49:29 -0400

    Successfully Finished complete.R

--- PA1/complete.R
+++ PA1/complete.R
@@ -5,9 +5,14 @@ complete <- function(directory, id = 1:332) {
     df <- data.frame(id=numeric(), nobs=numeric())
     
     for (i in id) {
-        
+        temp <- read.csv(fn[i])
+        obs <- nrow(temp[complete.cases(temp[[2]], temp[[3]]), ])
+        nr <- data.frame(id=i,nobs=obs)
+        df <- rbind(df,nr)
     }
     
     setwd(wd)
+    
+    df
     
 }
